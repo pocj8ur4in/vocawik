409a53532bd7958a300376ddedd2fad54b8c1a25
pocj8ur4in <pocj8ur4in@gmail.com>
2026-02-17 22:20:57 +0000

    fix(backend): remove security item on swagger ui

--- backend/src/main/java/com/vocawik/config/SwaggerConfig.java
+++ backend/src/main/java/com/vocawik/config/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.vocawik.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -41,7 +40,6 @@ public class SwaggerConfig {
                                 .title(applicationName)
                                 .description(applicationDescription)
                                 .version(applicationVersion))
-                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .components(
                         new Components()
                                 .addSecuritySchemes(
